d89eb81adac5971b691115a201d6ea550f1c6019
mmmitton <mmmitton@calpoly.edu>
2025-03-16 01:12:10 -0700

    added stuff

--- Final_Project/app/src/main/java/com/zybooks/snackulator/ui/SnackApp.kt
+++
@@ -1,9 +0,0 @@
-package com.zybooks.snackulator.ui
-
-import androidx.compose.runtime.Composable
-
-
-@Composable
-fun SnackApp(){
-
-}
--- Final_Project/app/src/main/java/com/zybooks/snackulator/ui/StarbucksSceen.kt
+++
@@ -1,8 +0,0 @@
-package com.zybooks.snackulator.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun StarbucksScreen(){
-
-}
